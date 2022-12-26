78a54f4676f991ad9cb3a3b593a96893cfaa8582
Adrian <blackrebel.068@gmail.com>
2022-12-26 14:55:06 -0600

    created methods crud

--- server.js
+++ server.js
@@ -3,27 +3,54 @@ const morgan = require("morgan");
 
 const app = express();
 app.use(morgan("dev"));
+app.use(express.json());
 
 const PORT = 3000;
 
+let products = [{ id: 1, name: "laptop", price: 7500 }];
+
 app.get("/products", (req, res) => {
-  res.send("get products");
+  res.json(products);
 });
 
 app.post("/products", (req, res) => {
-  res.send("create a product");
+  const newProducts = { ...req.body, id: products.length + 1 };
+  products.push(newProducts);
+  res.send(newProducts);
 });
 
-app.put("/products", (req, res) => {
-  res.send("update a product");
+app.put("/products/:id", (req, res) => {
+  const newData = req.body;
+  const productFound = products.find((x) => x.id === parseInt(req.params.id));
+  if (!productFound)
+    return res.status(404).json({
+      message: "Product not found",
+    });
+
+  products = products.map((x) =>
+    x.id === parseInt(req.params.id) ? { ...x, ...newData } : x
+  );
+  res.json({ message: "Product updated" });
 });
 
-app.delete("/products", (req, res) => {
-  res.send("delete a product");
+app.delete("/products/:id", (req, res) => {
+  const productFound = products.find((x) => x.id === parseInt(req.params.id));
+  if (!productFound)
+    return res.status(404).json({
+      message: "Product not found",
+    });
+
+  products = products.filter((x) => x.id !== parseInt(req.params.id));
+  res.sendStatus(204);
 });
 
 app.get("/products/:id", (req, res) => {
-  res.send("get a product");
+  const productFound = products.find((x) => x.id === parseInt(req.params.id));
+  if (!productFound)
+    return res.status(404).json({
+      message: "Product not found",
+    });
+  res.json(productFound);
 });
 
 app.listen(PORT);
